4ddf55b862e578a0529c84fc32ed9024b22ed17b
周培发 <zhoupeifa@levect.com>
2018-08-10 15:07:42 +0800

    修改预算总表

--- client/budget/totalTable/index.tsx
+++ client/budget/totalTable/index.tsx
@@ -6,7 +6,6 @@ import { observer } from 'mobx-react';
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
-import rootStore from '../../store';
 import AdvancedSearch from '../manage/components/AdvancedSearch';
 import excellentexport from '../../components/excellentexport';
 import store from '../manage/store';
@@ -31,10 +30,47 @@ export default class extends Component {
         this.advancedSearchDisplay = false;
         document.removeEventListener('click', this.hideAdvancedSearch);
     }
+    state = {
+        columns: [] as any
+    }
+    componentDidMount() {
+        const columns = [
+            {
+                title: 2018,
+                width: 100,
+                dataIndex: 'head',
+                key: 'head',
+                fixed: 'left',
+                children: [
+                    {
+                        title: '总表',
+                        dataIndex: `total`,
+                        key: `total`,
+                    }
+                ],
+            } as any,
+        ]
+        for (let i = 0; i < 12; i++) {
+            const children = [{ key: 1, value: '预算' }, { key: 3, value: '占收入比例' }, { key: 5, value: '实际' }, { key: 7, value: '占收入比例' }, { key: 9, value: '预算完成率' }].map((key) => ({
+                id: key.key,
+                title: key.value,
+                dataIndex: `${key.key}_${i}月`,
+                key: `${key.key}_${i}月`,
+            }));
+            columns.push({
+                title: `${i + 1}月`,
+                dataIndex: `month${i}`,
+                key: `month${i}`,
+                children,
+            });
+        }
+        this.setState({ columns })
+    }
     exportExcel = () => {
         const table = document.getElementsByTagName('table')[0]
         excellentexport.excel(table, '工作簿1', '阿米巴')
     }
+
     public render() {
         return (
             <div>
@@ -45,6 +81,10 @@ export default class extends Component {
                         {this.advancedSearchDisplay && <AdvancedSearch store={store} />}
                     </SearchBar>
                 </Section>
+
+                <TableSection>
+                    <Table pagination={false} scroll={{ x: 'auto' }} rowKey="total" bordered size="small" dataSource={[{ total: '收入-阿米巴' }, { total: '成本费用-阿米巴' }, { total: '利润-阿米巴' }, { total: '收入-财务' }, { total: '成本费用-财务' }, { total: '利润-财务' }]} columns={this.state.columns} />
+                </TableSection>
             </div>
         );
     }
