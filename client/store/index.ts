d9f3b1c1bec26dd674fa22fa7adb23ab4dc612b9
liwei <ldw1216@foxmail.com>
2018-08-11 20:16:45 +0800

    将预算数据状态改成全局状态

--- client/store/index.ts
+++ client/store/index.ts
@@ -1,18 +1,14 @@
 import axios from 'axios';
 import { action, observable, toJS, when } from 'mobx';
 import User from 'store/User';
+import { ExpenseTypeList } from '../budget/model/ExpenceType';
+import Period from '../budget/model/Period';
+import { BudgetStore } from '../budget/store';
 
 export class Store {
     @observable public user = new User();
-    @observable public expenseTypes: amb.IExpenseType[] = [];
-    constructor() {
-        when(() => !!this.user._id, () => {
-            axios.get('/expense-type').then((res) => {
-                this.expenseTypes = res.data;
-            });
-        });
-        // 切换年度的时候重新读取当年的费用
-    }
+    @observable public expenseTypeStore = new ExpenseTypeList();
+    @observable public budgetStore = new BudgetStore();
 }
 
 export default new Store();
